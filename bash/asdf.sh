37ffae070b59bfe376295ef25171f7477b614a0c
Jon Wiedmann <jonwiedmann@gmail.com>
2013-07-10 21:59:12 -0400

    init commit

---
+++ bash/asdf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# %B = bold
+# %c = relative path
+# %h = ?? time based
+# %j = ??
+# %p = time
+# %U = underline
+# %w = month
+# %W = day
+# %y = year short
+# %Y = year long
+
+# → » 𝝣 ⫸ ♿ ☯ ☮
+
+# start Eclipse, Chrome
+Desktop/Link\ to\ eclipse &
+#Chrome/usr/bin/google-chrome &
+
+# terminal prompt
+set prompt="%B%c%b » "
+resize -s 33 90
+ 
+# working dir
+cd workspaceLinux/School/pj1
+clear
