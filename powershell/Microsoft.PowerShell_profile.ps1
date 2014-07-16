61098c58837989f575c662735d28e29c4bf05b67
Jon Wiedmann <jonwiedmann@gmail.com>
2014-07-16 18:05:07 -0400

    Update and rename profile.ps1 to Microsoft.PowerShell_profile.ps1

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+Set-Location C:\git
+#New-Alias acp git add -A && git commit -m "$1" && git push    #untested
+Clear-Host
+
+function prompt {
+	$host.ui.rawui.WindowTitle = $(Get-Location)
+  
+  #$curDir = Split-Path $(Get-Location)  -Leaf
+  $fullPath = "$(Get-Location)".Replace("\","/")
+  $curBranch = ""
+  $doubleArrow = [char]0xBB;
+  
+  if(Test-Path .git) {
+    $curBranch += " ("
+
+    git branch | foreach {
+      if ($_ -match "^\*(.*)"){
+        $curBranch += $matches[1].trimstart(" ")
+      }
+    }
+
+    $curBranch += ")"
+  }
+
+  Write-Host $fullPath -nonewline -foregroundcolor DarkCyan
+  Write-Host $curBranch -nonewline -foregroundcolor Gray
+  Write-Host ""
+  Write-Host $doubleArrow -nonewline -foregroundcolor DarkCyan
+  return " " 
+}
