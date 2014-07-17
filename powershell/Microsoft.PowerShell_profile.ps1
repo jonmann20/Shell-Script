60d07aff6750f5daba292ea8ee42fbe93a4b1636
Jon Wiedmann <jonwiedmann@gmail.com>
2014-07-17 14:30:34 -0400

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,41 @@
 Set-Location C:\git
-#New-Alias acp git add -A && git commit -m "$1" && git push   #untested
 Clear-Host
 
 function prompt {
-	#$curDir = Split-Path $(Get-Location)  -Leaf
- 	$fullPath = "$(Get-Location)".Replace("\","/")
- 	$host.ui.rawui.WindowTitle = $fullPath
+  #$curDir = Split-Path $(Get-Location)  -Leaf
+  $fullPath = "$(Get-Location)".Replace("\","/")
+  $host.ui.rawui.WindowTitle = $fullPath
 
- 	$curBranch = ""
- 	$doubleArrow = [char]0xBB;
+  $curBranch = ""
+  $doubleArrow = [char]0xBB;
   
- 	if(Test-Path .git) {
-    	$curBranch += " ("
+  if(Test-Path .git) {
+    $curBranch += " ("
 
-   		git branch | foreach {
-    		if ($_ -match "^\*(.*)"){
-        		$curBranch += $matches[1].trimstart(" ")
-      		}
-    	}
+    git branch | foreach {
+      if ($_ -match "^\*(.*)"){
+        $curBranch += $matches[1].trimstart(" ")
+      }
+    }
 
-    	$curBranch += ")"
-	}
+    $curBranch += ")"
+  }
 
-  	Write-Host $fullPath -nonewline -foregroundcolor DarkCyan
-  	Write-Host $curBranch -nonewline -foregroundcolor Gray
-  	Write-Host ""
-  	Write-Host $doubleArrow -nonewline -foregroundcolor DarkCyan
-  	return " " 
+  Write-Host $fullPath -nonewline -foregroundcolor DarkCyan
+  Write-Host $curBranch -nonewline -foregroundcolor DarkYellow
+  Write-Host ""
+  Write-Host $doubleArrow -nonewline -foregroundcolor DarkCyan
+  return " " 
+}
+
+function acp {
+  cmd /c git add -A; git commit -m "$args"; git push
+}
+
+function pgb {
+  phonegap build android
+}
+
+function pgr {
+  phonegap run android
 }
