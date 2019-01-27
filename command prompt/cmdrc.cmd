7fb20740017cab3a0294756610b01b1afc2e9a73
Jon Wiedmann <jonwiedmann@gmail.com>
2019-01-27 17:37:23 -0500

    Update cmdrc.cmd

--- command prompt/cmdrc.cmd
+++ command prompt/cmdrc.cmd
@@ -1,28 +1,30 @@
-:: Install this to C:\
+:: Install this file to C:\
 
 @echo off
 
-:: Prompt
-::git.exe %*
-::set GITBRANCH=for /f "tokens=2" %%I in ('git.exe branch 2^> NUL ^| findstr /b "* "') do set GITBRANCH=%%I
-::echo %GITBRANCH%
-
-set prompt=$p$_$g
-
 :: Aliases
-DOSKEY ls=dir /B
-DOSKEY subl="C:\Program Files\Sublime Text 3\sublime_text.exe"
-DOSKEY ..=cd ..
-DOSKEY ...=cd ..\..
+DOSKEY ls = dir /B
+DOSKEY subl = "C:\Program Files\Sublime Text 3\sublime_text.exe"
+DOSKEY .. = cd ..
+DOSKEY ... = cd ..\..
 
 :: git
-DOSKEY br=git branch
-DOSKEY st=git status -s
-DOSKEY ch=git checkout $1
-DOSKEY lg=git log --graph
-DOSKEY ac=git add -A $T git commit -m "$*"
-DOSKEY acp=git add -A $T git commit -m "$*" $T git push
+DOSKEY br = git branch
+DOSKEY st = git status -s
+DOSKEY ch = git checkout $1
+DOSKEY lg = git log --graph
+DOSKEY ac = git add -A $T git commit -m "$*"
+DOSKEY acp = git add -A $T git commit -m "$*" $T git push
 
 :: Startup
 cd C:\workspace
 dir
+
+:: Prompt
+set prompt=$E[7;32;47m$p$_$g$E[0m
+
+::git rev-parse --abbrev-ref HEAD > C:\workspace\gitbranch.txt
+::set /p branch=<C:\workspace\gitbranch.txt
+
+::set prompt=$p $E[7;32;47m%branch%$E[0m $_$g
+::DOSKEY cd = cd $* $T set prompt=$p
